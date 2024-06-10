900834d66ef3bae61639be064b75be3534d5aa6e
zimmy125 <zimmy125@yahoo.com>
2024-06-10 08:58:22 -0500

    Setup eslint for code

---
+++ 03-jscli/eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+
+export default [
+    js.configs.recommended,
+
+   {
+       languageOptions: {
+            globals: {
+                "console" : "readonly",
+            },
+        },
+ 
+        rules: {
+           "no-unused-vars": "warn",
+           "no-undef": "warn",
+           semi: "error",
+       },
+
+       files: ["**/tfswrapper"],
+   }
+];
